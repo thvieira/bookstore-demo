be27eee89595b3800bd2e60191245502948dea00
Thomas Vieira <thvieira.rs@gmail.com>
2022-10-10 23:42:07 -0300

    initial

---
+++ src/App.vue
@@ -0,0 +1,57 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+// import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+
+<header>
+  <h1>Bookstore Demo</h1>
+  <p>With a <b>flexible</b> layout.</p>
+</header>
+
+<nav>
+  <RouterLink to="/">Home</RouterLink>
+  <RouterLink to="#">Link</RouterLink>
+  <RouterLink to="#">Link</RouterLink>
+  <RouterLink to="#">Link</RouterLink>
+  <RouterLink to="#">Link</RouterLink>
+</nav>
+
+<main class="row">
+  <aside>
+    <h2>About Me</h2>
+    <h5>Photo of me:</h5>
+    <div class="fakeimg" style="height:200px;">Image</div>
+    <p>Some text about me in culpa qui officia deserunt mollit anim..</p>
+    <h3>More Text</h3>
+    <p>Lorem ipsum dolor sit ame.</p>
+    <div class="fakeimg" style="height:60px;">Image</div><br>
+    <div class="fakeimg" style="height:60px;">Image</div><br>
+    <div class="fakeimg" style="height:60px;">Image</div>
+  </aside>
+  <section class="main">
+    <h2>TITLE HEADING</h2>
+    <h5>Title description, Dec 7, 2017</h5>
+    <div class="fakeimg" style="height:200px;">Image</div>
+    <p>Some text..</p>
+    <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+    <br>
+    <h2>TITLE HEADING</h2>
+    <h5>Title description, Sep 2, 2017</h5>
+    <div class="fakeimg" style="height:200px;">Image</div>
+    <p>Some text..</p>
+    <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+  </section>
+</main>
+
+  <footer class="footer">
+    <h2>Footer</h2>
+  </footer>
+
+  <RouterView />
+</template>
+
+<style scoped>
+
+</style>
